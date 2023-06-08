d261efdadcaa47b60cfbd328e00af442297a197f
Aleksandr Andrukhov <drammtv@gmail.com>
2023-06-08 12:55:39 +0000

    Added concatination of music

--- run.sh
+++ run.sh
@@ -18,23 +18,28 @@ g=2
 
 echo "Clip prepared"
 
-while true;
-do
+music_paths=()
+
+#while true;
+#do
   for music_path in $music_folder/**/*.*;
   do
     file_name=$(basename "$music_path")
     file_name="${file_name%.*}"
     file_name=$(sed -r 's/^[0-9]+.//' <<< $file_name)
 
-    echo $file_name
+    music_paths+=("$music_path")
+  done
+
+  music_paths_str=$(printf "%s|" "${music_paths[@]}")
 
     # https://stackoverflow.com/questions/33684845/using-ffmpeg-to-stream-my-webcams-video-to-youtube
 
-    ffmpeg -re -loop 1 -framerate $fps -i "$image_path" -i "$music_path" -shortest \
+    ffmpeg -hide_banner -re -loop 1 -framerate $fps -i "$image_path" -i "concat:$music_paths_str" -shortest \
     -c:v libx264 -preset fast -tune zerolatency -b:v 4000k -minrate 3000k -maxrate 5000k -bufsize 2M -pix_fmt yuv420p -g $g \
     -vf "scale=$video_w:$video_h, drawtext=text='$file_name':x=$offset_w:y=$video_h-$fontsize-$offset_h:fontsize=$fontsize:fontcolor=$fontcolor:bordercolor=$bordercolor:borderw=1:" \
     -c:a aac -b:a 192k -ac 2 -ar 44100 \
     -f flv rtmp://a.rtmp.youtube.com/live2/fc25-t64w-zhka-pwth-693h \
-    -loglevel info -hide_banner
-  done
-done
+    -loglevel info
+#  done
+#done
