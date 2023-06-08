1fb5f860d83c8feb583b8eda363b966586da278e
Aleksandr Andrukhov <drammtv@gmail.com>
2023-06-08 09:09:44 +0000

    Optimised framerates, shortest and g for static background

--- best_version.sh
+++ best_version.sh
@@ -11,8 +11,8 @@ offset_w=15
 offset_h=15
 fontcolor="#ffffff@1.0"
 bordercolor="#000000@1.0"
-fps=24
-g=24
+fps=2
+g=2
 # ffmpeg -loop 1 -framerate 24 \
 # -i "$image_path" -c:v libx264 -preset veryslow -tune stillimage -pix_fmt yuv420p -t 5 -crf 24 -movflags +faststart "$video_path"
 
@@ -29,11 +29,12 @@ do
     echo $file_name
 
     # https://stackoverflow.com/questions/33684845/using-ffmpeg-to-stream-my-webcams-video-to-youtube
-    
-    ffmpeg -re -loop 1 -i "$image_path" -i "$music_path" \
-    -c:v libx264 -preset ultrafast -tune zerolatency -b:v 5000k -minrate 4500k -maxrate 6000k -bufsize 2M -pix_fmt yuv420p -g $g \
+
+    ffmpeg -re -loop 1 -framerate $fps -i "$image_path" -i "$music_path" -shortest \
+    -c:v libx264 -preset fast -tune zerolatency -b:v 4000k -minrate 3000k -maxrate 5000k -bufsize 2M -pix_fmt yuv420p -g $g \
     -vf "scale=$video_w:$video_h, drawtext=text='$file_name':x=$offset_w:y=$video_h-$fontsize-$offset_h:fontsize=$fontsize:fontcolor=$fontcolor:bordercolor=$bordercolor:borderw=1:" \
     -c:a aac -b:a 192k -ac 2 -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/5f68-6rp4-g6gy-8cc7-ae11
+    -f flv rtmp://a.rtmp.youtube.com/live2/fc25-t64w-zhka-pwth-693h \
+    -loglevel info -hide_banner
   done
 done
